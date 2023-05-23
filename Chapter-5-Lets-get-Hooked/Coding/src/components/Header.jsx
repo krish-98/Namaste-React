308c5f9f39d0568a8af2d886282666bca95c8c32
krish-98 <muralikrishnanm98@gmail.com>
2023-05-23 22:52:47 +0530

    chapter-5 coding assignment done

---
+++ Chapter-5-Lets-get-Hooked/Coding/src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      <img src="" alt="" />
+      <h1></h1>
+
+      <ul>
+        <li>hi</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </header>
+  )
+}
+export default Header
